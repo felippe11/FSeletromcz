dc8b9a97deb804a727fa578224b507ac88829e19
felippe11 <andrefelippe11@hotmail.com>
2025-05-01 23:31:07 -0300

    contato por email

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import create_app
+import os
+
+# Definir a variável de ambiente para desabilitar o carregamento automático do .env pelo Flask
+os.environ["FLASK_SKIP_DOTENV"] = "1"
 
 app = create_app()
 
